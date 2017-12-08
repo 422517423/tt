0c08b00474813228a7dbd3e399f3c5441b70a2b7
Administrator <422517423@qq.com>
2017-12-08 17:38:17 +0800

    提交新的了啊

--- src/TT.java
+++ src/TT.java
@@ -2,5 +2,6 @@ public class TT {
     public static void main(String[] args) {
         System.out.println(33333);
         System.out.println("我的啊");
+        System.out.println("又有新的了啊");
     }
 }
